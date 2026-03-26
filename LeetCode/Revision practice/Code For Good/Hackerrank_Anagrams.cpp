d0d94a5987e16030ea76b0bf3edcd7603fe0943b
Sainava <sainava.modak@gmail.com>
2026-03-26 14:30:42 +0530

    hackerrank Anagrams

---
+++ LeetCode/Revision practice/Code For Good/Hackerrank_Anagrams.cpp
@@ -0,0 +1,150 @@
+/*
+Two words are anagrams of one another if their letters can be rearranged to form the other word.
+
+Given a string, split it into two contiguous substrings of equal length. Determine the minimum number of characters to change to make the two substrings into anagrams of one another.
+
+Example
+
+Break  into two parts: 'abc' and 'cde'. Note that all letters have been used, the substrings are contiguous and their lengths are equal. Now you can change 'a' and 'b' in the first substring to 'd' and 'e' to have 'dec' and 'cde' which are anagrams. Two changes were necessary.
+
+Function Description
+
+Complete the anagram function in the editor below.
+
+anagram has the following parameter(s):
+
+string s: a string
+Returns
+
+int: the minimum number of characters to change or -1.
+Input Format
+
+The first line will contain an integer, , the number of test cases.
+Each test case will contain a string .
+
+Constraints
+
+
+ consists only of characters in the range ascii[a-z].
+Sample Input
+
+6
+aaabbb
+ab
+abc
+mnop
+xyyx
+xaxbbbxx
+Sample Output
+
+3
+1
+-1
+2
+0
+1
+Explanation
+
+Test Case #01: We split  into two strings ='aaa' and ='bbb'. We have to replace all three characters from the first string with 'b' to make the strings anagrams.
+
+Test Case #02: You have to replace 'a' with 'b', which will generate "bb".
+
+Test Case #03: It is not possible for two strings of unequal length to be anagrams of one another.
+
+Test Case #04: We have to replace both the characters of first string ("mn") to make it an anagram of the other one.
+
+Test Case #05:  and  are already anagrams of one another.
+
+Test Case #06: Here S1 = "xaxb" and S2 = "bbxx". You must replace 'a' from S1 with 'b' so that S1 = "xbxb".
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include<fstream>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'anagram' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int anagram(string s) {
+    // Brute force 
+    int n = s.size() ;
+    int cnt = 0 ;
+    
+    if(n % 2 != 0) return -1 ;
+    
+    vector<int> firstHalf(26,0);
+    vector<int> secondHalf(26,0);
+    
+    for(int i = 0 ; i < n/2 ; i++){
+        firstHalf[s[i]-'a']++;
+    }
+    
+    for(int i = n/2 ; i < n ; i++){
+        secondHalf[s[i]-'a']++;
+    }
+        
+    for(int i = 0; i < 26 ; i++){
+        if (firstHalf[i] != secondHalf[i]){
+            cnt += abs(firstHalf[i] - secondHalf[i]);
+        }
+    }
+    
+    // TC:O(n) and SC:O(1)
+    return cnt /2;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = anagram(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char c) { return !isspace(c); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char c) { return !isspace(c); }).base(),
+        s.end()
+    );
+
+    return s;
+}
