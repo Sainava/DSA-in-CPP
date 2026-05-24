6662dffc646c8e9ff32a7be9fca5e28a695e4326
Sainava <sainava.modak@gmail.com>
2026-05-25 01:29:10 +0530

    Jump Game

---
+++ Neetcode 150/123.Jump_Game.cpp
@@ -0,0 +1,45 @@
+/*
+You are given an integer array nums where each element nums[i] indicates your maximum jump length at that position.
+
+Return true if you can reach the last index starting from index 0, or false otherwise.
+
+Example 1:
+
+Input: nums = [1,2,0,1,0]
+
+Output: true
+Explanation: First jump from index 0 to 1, then from index 1 to 3, and lastly from index 3 to 4.
+
+Example 2:
+
+Input: nums = [1,2,1,0,1]
+
+Output: false
+Constraints:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 1000
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int n = nums.size() ; // maxIndex reachable till now 
+        int maxIndex = 0 + nums[0] ;
+
+        for(int i = 1 ; i < n ; i++){
+            if(maxIndex < i){
+                // This index cannot be reached , cannot go further 
+                return false ; 
+            }
+            maxIndex = max(maxIndex , i + nums[i]);
+        } 
+        // TC : O(n) and SC:O(1)
+        return true ; 
+        
+    }
+};
