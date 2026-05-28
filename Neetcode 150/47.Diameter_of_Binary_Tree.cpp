3626f5cbf92a23a1b3e30497153f8b48c4b72496
Sainava <sainava.modak@gmail.com>
2026-05-29 01:15:32 +0530

    Diameter of Tree Alternate Approach

--- Neetcode 150/47.Diameter_of_Binary_Tree.cpp
+++ Neetcode 150/47.Diameter_of_Binary_Tree.cpp
@@ -62,3 +62,37 @@ public:
         return diameterHelper(root).first - 1; // -1 since we are counting nodes but we need edges
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int helper(TreeNode* root , int &maxi){
+        if( root == NULL){
+            return 0 ; 
+        }  
+
+        int left = helper(root->left , maxi );
+        int right = helper(root->right , maxi ) ; 
+
+        maxi = max( maxi ,1 + left + right ) ;
+
+        return 1 + max(left , right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi = 0 ; 
+        helper ( root , maxi ) ;
+        //TC:O(n) and SC:O(n)
+        return maxi - 1 ;  // -1 as we count nodes but they want edges
+    }
+};
