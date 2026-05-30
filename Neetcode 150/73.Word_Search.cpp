6495f5aa99bc960e3541af6e9a88ffa8fb375d09
Sainava <sainava.modak@gmail.com>
2026-05-31 01:40:21 +0530

    Word search alternate

--- Neetcode 150/73.Word_Search.cpp
+++ Neetcode 150/73.Word_Search.cpp
@@ -103,3 +103,55 @@ public:
         return isPresent ;
     }
 };
+
+class Solution {
+public:
+    bool dfs(int row , int col ,int index ,vector<vector<char>>& board, string word ,vector<vector<bool>> &visited ,int m , int n){
+
+        if(index == word.size() - 1) return true;
+
+        visited[row][col]  = true  ;    
+
+        bool isPossible = false ; 
+        for(int delRow = -1 ; delRow <= 1 ; delRow++){
+            for(int delCol = -1 ; delCol <= 1 ; delCol++){
+                if( abs(delRow)+ abs(delCol) == 1){
+                    int r = row+delRow ;
+                    int c = col + delCol ; 
+
+                    if( r>= 0 && r<m && c>= 0 && c<n && !visited[r][c] && index + 1 < word.size() && board[r][c]== word[index + 1]){
+                        isPossible = dfs(r , c,index + 1 , board , word ,  visited ,m ,n);
+                    }
+                   if(isPossible){
+                        visited[row][col]  = false ; 
+                        return true ; 
+                   } 
+                }
+            }
+        }
+        visited[row][col]  = false ; 
+        return isPossible ; 
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size() ; 
+
+        
+        vector<vector<bool>> visited(m , vector<bool>(n,false)); // Will bactrack 
+        bool isPresent = false ; 
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(board[i][j] == word[0]){
+                    
+                    isPresent = dfs(i , j,0 , board , word ,  visited ,m ,n);
+                }
+                if(isPresent) return true ; 
+            }
+        }
+
+        // TC:O(R * C * 4^L) as for each cell (which there are R*C of) we go 4 directions till the last level L
+        // SC:O(R*C + L) R*C for the visited and L for the recursion stack as L levels 
+        return false ; 
+    
+    }
+};
